62cc1d37d4f0a927bdf6987a5e20eeecde87b1ab
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-02-14 20:47:22 -0600

    added retrospective

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -4,6 +4,12 @@ import axios from "axios";
 import UserCard from "./Components/UserCard";
 import styled from "styled-components";
 
+const Step5 = styled.div`
+  padding: 3%;
+  margin: 2%;
+  font-weight: bold;
+`;
+
 class App extends React.Component {
   state = {
     users: [],
@@ -38,6 +44,18 @@ class App extends React.Component {
         <h1>Shawn Batson's GitHub Followers!</h1>
         <h2>Followers</h2>
         <UserCard followers={this.state.followers} user={this.state.users} />
+
+        <Step5>
+          <p>
+            This is my retrospective: I wish I would have been able to just hit
+            the ground running, instead, I was able to use the foundation of
+            another resource and got into my groove then. I really do appreciate
+            hooks and functional components, so I will never take advantage of
+            them again. I feel that hooks are much more clean, and less explicit
+            binding required, so that it is much easier to follow the train of
+            logic without getting lost.{" "}
+          </p>
+        </Step5>
       </div>
     );
   }
